83ca41e543ae0a7cbfe9cadeccfcd15a11e46a0d
Opeyemi <yemi.otu@outlook.com>
2023-12-19 10:19:33 -0700

    data types

---
+++ data_types.py
@@ -0,0 +1,11 @@
+txt1 = "Hello World, test function"
+txt2 = "Hello World, testing function, test"
+if txt1.endswith("test"):
+    print("found test")
+else:
+    print("no test"),
+
+if txt2.endswith("test"):
+    print("found test")
+else:
+    print("no test")
